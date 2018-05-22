d16a0b25570c4284427650b674a0ad82db5c0478
wfrei020 <wfrei@gmail.com>
2018-05-22 02:19:21 -0400

    version 0.1

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # hackerYouChallenge
+
+This challenge is meant for HackerYou etntry to Full stack master course
+
+## Usefull information
+
+I maintained the file system structure from create-react-app build
+
+
+beau-hyc/src/  main portion of my code
--- beau-hyc/src/App.js
+++ beau-hyc/src/App.js
@@ -1,21 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './images/beaus-beige-logo.svg';
 import './App.css';
+import './LcboProductApi.js';
+import LcboProductApi from './LcboProductApi.js';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {products: [],
+        };
+    }
+      componentDidMount(){
+      this.LcboProducts();
+  }
+  
+  
+      LcboProducts(){
+      fetch({url:"https://lcboapi.com/Products",
+                        headers:{'Authorization':
+                        'MDphNjhjNGUxZS01Yjc1LTExZTgtYjkzMy01M2EwZDBjMGQ1NDM6ako4YTU4RDRvU2tVanBHR0NjYmM5VGFUZzI3YVJ2QXBLbkw5'
+      }})
+   .then(({ results }) => this.setState({ products: results }));
+                        
+      
+  }
   render() {
+      
+      
+      
+      
+      
+      
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="beau-main">
+        <header className="beau-header">
+          <img src={logo} className="beau-logo" alt="logo" />
+          <h1 className="beau-title">Summer Time</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="beau-intro">
+          Canadians' experience a long and endless winter, why not enjoy the short summer
+          with Beau's Seasonal Beer crafted for a wonderful and unique experience.  Our certified organic beer, prepared with honest consideration
+          to our beautiful evironment and our local community, and delivered
+          with a sense of friendly relationships. 
         </p>
+        <div><LcboProductApi /></div>
+        
       </div>
+      
+      /*
+      <div id="layout-content" className="layout-content-wrapper">
+        <div className="panel-list">{ products }</div>
+      </div> */
     );
   }
+  
+
+ 
+
+  
+  
 }
+
+
+
+
 
 export default App;
